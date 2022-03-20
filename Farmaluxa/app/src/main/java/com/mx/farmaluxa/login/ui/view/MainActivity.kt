ca78ae553d0d01367959226e94121199e3647735
vmddlr <systemsteamv1@gmail.com>
2022-03-19 18:55:53 -0600

    Maquetado de la pantalla de login

--- Farmaluxa/app/src/main/java/com/mx/farmaluxa/login/ui/view/MainActivity.kt
+++ Farmaluxa/app/src/main/java/com/mx/farmaluxa/login/ui/view/MainActivity.kt
@@ -3,11 +3,20 @@ package com.mx.farmaluxa.login.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mx.farmaluxa.login.R
+import com.mx.farmaluxa.login.ui.view.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        this.showLoginView()
+    }
+
+    private fun showLoginView() {
+        val fragmentManager = this.supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.mainContainerNav, LoginFragment.newInstance() )
+        transaction.commit()
     }
 }
--- Farmaluxa/app/src/main/java/com/mx/farmaluxa/login/ui/view/login/LoginFragment.kt
+++ Farmaluxa/app/src/main/java/com/mx/farmaluxa/login/ui/view/login/LoginFragment.kt
@@ -1,22 +1,77 @@
 package com.mx.farmaluxa.login.ui.view.login
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mx.farmaluxa.login.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.mx.farmaluxa.login.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    private lateinit var bindin : FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
+
+    companion object {
+
+        fun newInstance(): LoginFragment {
+            val fragment = LoginFragment()
+
+            val args = Bundle()
+
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        this.binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return this.binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.binding.btnLogin.setOnClickListener {
+            openWhatsApp()
+        }
+    }
+
+    private fun openWhatsApp() {
+        val installed: Boolean = isWhatsAppInstalled()
+
+
+        if (installed) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data =
+                Uri.parse("http://api.whatsapp.com/send?phone=$+527737367499&text=Hola, me gustaría registrarme para conocer más sobre tu catálogo de productos")
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this.requireContext(),
+                com.mx.farmaluxa.sharedutil.R.string.whatsapp_is_not_installed,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun isWhatsAppInstalled(): Boolean {
+
+        val pm: PackageManager = this.requireContext().packageManager
+
+        val isInstalled: Boolean = try {
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+        return isInstalled
+
     }
 }
