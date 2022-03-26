ae195c224f95b4b19ab91d4eb35bfba5aa4a6046
Gustavo Hernandez <ghernandezp@intercam.com.mx>
2022-03-26 14:24:08 -0600

    Draw Menu
    -- Se agrega GLIDE
    -- Se agrega DRAW MENU
    -- Se agregan iconos

--- Farmaluxa/warehouse/src/main/java/com/mx/farmaluxa/warehouse/ui/view/WarehouseActivity.kt
+++ Farmaluxa/warehouse/src/main/java/com/mx/farmaluxa/warehouse/ui/view/WarehouseActivity.kt
@@ -1,12 +1,54 @@
 package com.mx.farmaluxa.warehouse.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.mx.farmaluxa.warehouse.R
+import com.mx.farmaluxa.warehouse.databinding.ActivityWarehouseBinding
 
 class WarehouseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWarehouseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_warehouse)
+        binding = ActivityWarehouseBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        this.configureToolBar()
+    }
+
+    private fun configureToolBar() {
+        val navHostFragment =
+            this.supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph, this.binding.drawerLayout)
+        this.binding.iHeaderLayout.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        this.binding.iHeaderLayout.toolbar.title = getString(com.mx.farmaluxa.sharedutil.R.string.app_name)
+        this.binding.iHeaderLayout.tvTitle.text = getString(R.string.str_title_module)
+
+        this.configureDrawMenu()
+    }
+
+    private fun configureDrawMenu() {
+        this.binding.iNavViewLayout.tvUserName.text = "Gustavo Hernandez Parra"
+        this.binding.iNavViewLayout.tvEmployeeId.text = "#00001"
+        this.binding.iNavViewLayout.chpEmployeeRol.text = getString(R.string.str_warehouse_rol)
+        
+
+        this.binding.iNavViewLayout.btnClose.setOnClickListener {
+            // Todo - Realizar Servicio de Logout
+            this.finish()
+        }
     }
 }
